302d6544a6d90bc7fb83e5e67fcb86bf7f64b29c
Eugene7182 <Senzove@mail.ru>
2025-09-04 21:35:42 +0500

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,8 @@
-// frontend/src/pages/Dashboard.tsx
 export default function Dashboard() {
   return (
     <div style={{ padding: 24 }}>
       <h2>Dashboard</h2>
       <p>Вы авторизованы. Токен лежит в localStorage.</p>
     </div>
-  );
+  )
 }
